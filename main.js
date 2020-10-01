caf0a3418504e329395f71dfd79c808582655131
Myck <myck.pessoal@gmail.com>
2020-10-01 16:02:31 -0300

    check

--- main.js
+++ main.js
@@ -11,7 +11,13 @@ form.addEventListener('submit', (e) =>{
 
 });
 
-function checkInputs(){
+function checkInputs() {
+  const verification = {
+    usernameValue: false,
+    emailValue: false,
+    passwordValue: false,
+    };
+    
     //get the values from the inputs
     const usernameValue = username.value.trim();
     const emailValue = email.value.trim();
@@ -22,11 +28,13 @@ function checkInputs(){
         //Show error and add error class
         setErrorFor(username, 'Username cannot be blank');
     }else{
+        verification.usernameValue = true;
         setSuccessFor(username);
     }
     if(emailValue === ""){
         setErrorFor(email, 'Email cannot be blank');
     }else{
+        verification.emailValue = true;
         setSuccessFor(email);
     }
     if(passwordValue === ""){
@@ -39,10 +47,15 @@ function checkInputs(){
     }else if(repeatPassValue != passwordValue){
         setErrorFor(repeatPass, 'Password does not match');
     }else{
+        verification.passwordValue = true;
         setSuccessFor(repeatPass);
     }
-    
-    alert("Everithing is fine! You are registered!")
+  const auth = Object.values(verification)
+      .every((item) => item === true);
+  // call authentication
+  if (auth) {
+    // auth
+  }
 }
 
 function setErrorFor(input, message){
@@ -57,10 +70,8 @@ function setErrorFor(input, message){
     formControl.classList.add('error');
 }
 
+
 function setSuccessFor(input){
     const formControl = input.parentElement;
     formControl.className = "form-control success";
-
-    //add messa message
-    
 }
